0deeb87bd8f9bc80dfcf460abb8be3795126053c
Matt Snyder <matt.snyder@yahoo.com>
2020-09-10 17:05:00 -0400

    loading symbol during login

--- app/login.component.ts
+++ app/login.component.ts
@@ -3,6 +3,8 @@ import { RouterExtensions } from "nativescript-angular/router";
 import { HttpClient } from "@angular/common/http";
 import { NativeScriptFormsModule } from "@nativescript/angular/forms";
 import { alert, prompt } from "tns-core-modules/ui/dialogs";
+import { ActivityIndicator } from "tns-core-modules/ui/activity-indicator";
+import { EventData } from "tns-core-modules/data/observable";
 
 @Component({
     selector: "ns-login",
@@ -15,6 +17,7 @@ export class LoginComponent {
         username: "", 
         password: ""
     }
+    public isBusy: boolean = false;
     public confirm: string = "";
 
     constructor(
@@ -39,18 +42,22 @@ export class LoginComponent {
 
     submit() {
         if (this.isLoggingIn) {
+            this.isBusy = true;
             this.validateLogin().subscribe(res => {
                 let token = res.body; // return token if verification successful
                 this.routerExtensions.navigate(["items", token], { clearHistory: true });
             }, err => { // alert with error message if verification failed
                 let response = err.error.slice(7, err.error.length);
                 alert(response);
+                this.isBusy=false;
             });
         } else {
             if(this.credentials.password != this.confirm) {
                 alert("passwords do not match");
+                this.isBusy=false;
                 return;
             }
+            this.isBusy = true;
             this.createUser().subscribe(res => {
                 // if account creation successful: validate login and pass token
                 this.validateLogin().subscribe(res => {
@@ -64,7 +71,13 @@ export class LoginComponent {
                 let response = err.error.slice(7, err.error.length);
                 alert(response); 
             });
+            this.isBusy=false;
         }
+    }
+
+    onBusyChanged(args: EventData) {
+        let indicator: ActivityIndicator = <ActivityIndicator>args.object;
+        console.log("indicator.busy changed to: " + indicator.busy);
     }
 }
 
